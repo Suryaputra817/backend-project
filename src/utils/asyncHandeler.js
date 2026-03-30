395c87f7e6c2a2b6f5ace5597e936e11bc1f446a
Manjit Kumar Shyanti <manjitkumarshyanti@gmail.com>
2026-03-30 11:48:03 +0530

    feat: add cookie-parser and cors middleware, implement async error handling, and create ApiError and ApiResponse utility classes

---
+++ src/utils/asyncHandeler.js
@@ -0,0 +1,20 @@
+const asyncHandler = (requestHandeler) => {
+  return (req, res, next) => {
+    Promise.resolve(requestHandeler(req, res, next)).catch((err) => next(err));
+  };
+};
+
+export { asyncHandler };
+
+/*
+const asyncHandler = (fn) => async (req, res, next) => {
+  try {
+    await fn(req, res, next);
+  } catch (error) {
+    res.status(error.code || 500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+*/
